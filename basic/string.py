f879b05befec1c9b02b30fe997e94be97dffc7ca
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-25 18:21:01 +0530

    string in python

---
+++ basic/string.py
@@ -0,0 +1,17 @@
+var1 = 'hello world'
+var2 = "Python"
+var3 = '''Welcome'''
+
+print(var1[-1]) #d
+print(var1[-2]) #l
+print(var1[1:5]) #ello
+print(var1[5:-2]) # wor
+# print(var1[50]) # IndexError: string index out of range
+print(var1+var2)  #hello worldPython
+
+print(var1.lower()) 
+print(var1.upper()) 
+print(var1.find('l')) 
+print(var1.replace('hello','Hi')) 
+
+
