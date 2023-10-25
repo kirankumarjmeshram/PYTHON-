141370d8a377e013b341bd4ca5f4ce90494597d9
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-25 12:42:26 +0530

    cammand line arguments

---
+++ cmdLineArgv.py
@@ -0,0 +1,8 @@
+import sys
+print ("Number of arguments:", len(sys.argv),'arguments.')
+print("Argunent List:", str(sys.argv))
+x = int(sys.argv[1])
+y = int(sys.argv[2])
+
+z = x + y
+print("x = ",x, "Y = ",y, "Z =",z) # x =  20 Y =  30 Z = 50
