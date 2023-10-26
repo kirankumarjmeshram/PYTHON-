06c9b2ec65f6ace15f4d037362413e8db3aeece1
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-26 14:46:46 +0530

    dictionary

---
+++ basic/Dictionary.py
@@ -0,0 +1,22 @@
+# Strings, numbers, tuples as dict keys
+# dict fns
+    # str(dict) 
+    # len(dict)
+    # type (variable)
+
+dict = {'1':'Mumbai', '2':'Pune', '3':'Nagpur'}
+print(len(dict))
+print(str(dict)) # gives in string format
+print(type(dict)) #<class 'dict'>
+
+# Dictonary METHODS
+    # dict.clear()
+    # dict.copy()
+    # dict.fromkeys()
+    # dict.get(key, default=None)
+    # dict.has(key)
+    # dict.items()
+    # dict.keys()
+    # dict.setdefault(key, defaulf=None)
+    # dict.update((dict2))
+    
