10b63027decd1a0e753221c7fa2e63c2c2d52022
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-26 12:05:25 +0530

    for  loop for dict

--- a.py
+++ a.py
@@ -1,3 +1,14 @@
-a = 20
-b=a
-print(id(a), id(b))
+# a = 20
+# b=a
+# print(id(a), id(b))
+
+dict = {'1':'Mumbai', '2':'Pune', '3':'Nagpur'}
+
+for key in dict:
+    print(dict[key])
+
+for key, value in dict.items():
+    print(key, value)
+
+for key in dict.keys():
+    print (key)
