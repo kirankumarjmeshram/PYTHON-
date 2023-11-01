589bfc2fa65deb0be7a011a543ebb953a91f99a8
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-11-01 17:56:54 +0530

    slicer

--- basic/basic1.py
+++ basic/basic1.py
@@ -1,5 +1,14 @@
 # User Input: The input() function allows user input. 
 # It takes the line entered on a console and convert it into a string & returns it
 
-fname = input('Fname: ')
-print(fname)
+# fname = input('Fname: ')
+# print(fname)
+
+
+a = [1,2,3,4,5,6,7,8,9]
+
+print(a[0:4]) #[1, 2, 3, 4] 
+print(a[0:7:2]) #[start:stop:step] # [1, 3, 5, 7]
+print(a[0:7:1]) # [1, 2, 3, 4, 5, 6, 7]
+print(a[4:2:-1]) # [5,4]
+print(a[::-1]) #[9, 8, 7, 6, 5, 4, 3, 2, 1]
