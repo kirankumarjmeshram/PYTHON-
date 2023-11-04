34c7fa6ad94da7f8c7810419f03e23115823609f
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-11-04 18:30:34 +0530

    object attribute and method

---
+++ oops/2_ObjectAttributeAndMethods.py
@@ -0,0 +1,21 @@
+class Dog():
+    #  Class object attribute 
+    # same for any instance of a class
+    species = 'mammal'
+    def __init__(self, breed, name, spots):
+        #Attribute
+        #We take in the argument
+        #Assign it using self.attribute_name(=breed)
+        self.breed = breed
+        self.name = name
+
+        # Expect boolean True/False
+        self.spots = spots
+
+myDog = Dog(breed='labra', name='Sammy', spots=False)
+
+print(type(myDog)) #__main__.Dog
+print(myDog.breed) #labra
+print(myDog.name)  #Sammy
+print(myDog.spots) #False
+print(myDog.species) #mammal
