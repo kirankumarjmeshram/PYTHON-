04a82c3cd64c47e51bc3ceaba3d4687d97a56761
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-11-26 19:18:19 +0530

    execute many ie insert many

--- SQLite/database.py
+++ SQLite/database.py
@@ -14,10 +14,17 @@ c = conn.cursor()
 # )""")
 
 # c.execute("""CREATE TABLE customers (  first_name DATATYPE, last_name DATATYPE, email DATATYPE)""")
+
 #insert one record at a time
 c.execute("INSERT INTO customers VALUES ('Kirankumar J','M','kiranjm@gmail.com')")
 
-
+#insert many record at a time
+many_customers = [
+                  ('first_name1','last_name1','name1@gmail.com'),
+                  ('first_name2','last_name2','name2@gmail.com'),
+                  ('first_name3','last_name3','name3@gmail.com')
+                  ]
+c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
 
 print("Command executed successfully...")
 
