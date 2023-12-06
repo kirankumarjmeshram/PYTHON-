7e71a97f898e1d7f0461faa66ebc803cc70cf814
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-12-06 12:56:19 +0530

    python shutil module

---
+++ Modules/sh_util.py
@@ -0,0 +1,12 @@
+import shutil
+import os
+
+# copy file
+# shutil.copy("t1.txt", "t2.txt")
+# copy folder
+# shutil.copytree("file","file2")
+# move
+# shutil.move("file/t1.txt","file2")
+# remove
+# shutil.rmtree("file")
+# os.remove("t2.txt")
