d557d50595123d6a3be377d648a523ac0c9e9e54
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-01-01 16:12:40 +0530

    subprocess - run cmd cammds from script

---
+++ Modules/sub_process/sub_process.py
@@ -0,0 +1,5 @@
+# from subprocess import call
+# call(["python3", "/home/sarutobi/Documents/GitHub/PYTHON/Modules/sub_process/timer.py", "5"])
+
+import subprocess
+subprocess.run(["python3", "/home/sarutobi/Documents/GitHub/PYTHON/Modules/sub_process/timer.py", "5"])
