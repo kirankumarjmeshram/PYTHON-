974f1b27d6d43038f66a397c1189dfa60bb17d6a
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-01-02 18:37:10 +0530

    subprocess

--- Modules/sub_process/sub_process.py
+++ Modules/sub_process/sub_process.py
@@ -2,4 +2,8 @@
 # call(["python3", "/home/sarutobi/Documents/GitHub/PYTHON/Modules/sub_process/timer.py", "5"])
 
 import subprocess
-subprocess.run(["python3", "/home/sarutobi/Documents/GitHub/PYTHON/Modules/sub_process/timer.py", "5"])
+# subprocess.run(["python3", "/home/sarutobi/Documents/GitHub/PYTHON/Modules/sub_process/timer.py", "5"])
+
+
+# run ant app from subprocessor
+subprocess.run(["app_directory"])
---
+++ Modules/sub_process/subprocess1.py
@@ -0,0 +1,10 @@
+# Basic Usage of subprocess With UNIX-Based Shells
+
+import subprocess
+# subprocess.run(["bash", "-c", "ls /usr/bin | grep pycode"])
+
+# subprocess.run(["ls /usr/bin | grep pycode"], shell=True)
+
+# subprocess.run(["pwsh", "-Command", "ls C:\RealPython"])
+
+
